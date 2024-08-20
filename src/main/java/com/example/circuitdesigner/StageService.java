7d6a02896b7bf12b7fd1f255b22e210c3d26f1e8
Jasel <patriajasel@gmail.com>
2024-08-20 14:55:35 +0800

    Secondary Stage (removed)

--- src/main/java/com/example/circuitdesigner/StageService.java
+++ src/main/java/com/example/circuitdesigner/StageService.java
@@ -4,34 +4,14 @@ import javafx.stage.Stage;
 
 public class StageService {
 
-    /* N O T E S
-
-        * Remove secondary stage
-
-     */
 
     private Stage primaryStage;
-    private Stage secondaryStage;
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
-    public void setSecondaryStage(Stage secondaryStage) {
-        this.secondaryStage = secondaryStage;
-    }
-
     public Stage getPrimaryStage() {
         return primaryStage; // Added method to get primary stage
-    }
-
-    public void closeBothStages() {
-        if(primaryStage != null) {
-            primaryStage.close();
-        }
-
-        if(secondaryStage != null) {
-            secondaryStage.close();
-        }
     }
 }
